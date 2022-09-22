cd1b2bdeac2ada3c7aecf8fdcca1b62dc2959aa2
Neale Taylor <nealeptaylor@gmail.com>
2022-09-22 13:57:02 -0400

    Initial Commit

---
+++ MedTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MedTracker
+//
+//  Created by Neale Taylor on 2022-09-22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
