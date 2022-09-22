1e31afcbfaaea1970026cd099a13b898619c3563
Neale Taylor <nealeptaylor@gmail.com>
2022-09-22 17:20:33 -0400

    add hard coded meds and dose times

--- MedTracker/Meds.swift
+++ MedTracker/Meds.swift
@@ -7,3 +7,21 @@
 
 import Foundation
 
+struct Meds {
+  let med: String
+  let time: String
+  
+  static let all: [Meds] =  [
+    Meds(med: "Concerta 18mg", time: "9am"),
+    Meds(med: "Vitamin D", time: "8am"),
+    Meds(med: "Vitamin C", time: "8am"),
+    Meds(med: "Creatine", time: "5pm"),
+    Meds(med: "Protein Suplement", time: "5pm"),
+  ]
+}
+
+extension Meds: CustomStringConvertible {
+  var description: String {
+    return "\"\(med)\" — \(time)"
+  }
+}
