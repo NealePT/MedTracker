ec33b700080f6e4f25c417b4e9650723fe7913e3
Neale Taylor <nealeptaylor@gmail.com>
2022-09-22 17:10:06 -0400

    add menu bar toggler

--- MedTracker/AppDelegate.swift
+++ MedTracker/AppDelegate.swift
@@ -9,6 +9,8 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let popover = NSPopover()
 
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
 
@@ -17,15 +19,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let medTime = "10am"
         print("\(medName) at \(medTime)")
     }
+    
+    @objc func togglePopover(_ sender: Any?) {
+      if popover.isShown {
+        closePopover(sender: sender)
+      } else {
+        showPopover(sender: sender)
+      }
+    }
+
+    func showPopover(sender: Any?) {
+      if let button = statusItem.button {
+        popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+      }
+    }
+
+    func closePopover(sender: Any?) {
+      popover.performClose(sender)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
         
         if let statusButton = statusItem.button {
             statusButton.image = NSImage(systemSymbolName: "pills.fill", accessibilityDescription: "Pills")
-            statusButton.action = #selector(showMeds)
+            statusButton.action = #selector(togglePopover(_:))
         }
-        constructMenu()
+        popover.contentViewController = MedsViewController.freshController()
 
     }
 
