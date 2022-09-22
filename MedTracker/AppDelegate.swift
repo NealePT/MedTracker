26fff79b954d0b390512638d7425f83c7b225323
Neale Taylor <nealeptaylor@gmail.com>
2022-09-22 16:23:06 -0400

    add hard coded values

--- MedTracker/AppDelegate.swift
+++ MedTracker/AppDelegate.swift
@@ -10,11 +10,22 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
 
+    @objc func showMeds(_ sender: Any?) {
+        let medName = "Concerta 18mg"
+        let medTime = "10am"
+        print("\(medName) at \(medTime)")
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(systemSymbolName: "pills.fill", accessibilityDescription: "Pills")
+            statusButton.action = #selector(showMeds)
+        }
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
