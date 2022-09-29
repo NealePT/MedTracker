7e9785436c4a2906b003295df02cd99196ff904a
Neale Taylor <nealeptaylor@gmail.com>
2022-09-29 13:24:37 -0400

    adjust meds display

--- MedTracker/Meds.swift
+++ MedTracker/Meds.swift
@@ -8,18 +8,16 @@
 import Foundation
 
 struct Meds: Codable {
-//  let name: String
-//  let time: String
-//
-//  static let all: [Meds] =  [
-//    Meds(name: "Concerta 18mg", time: "9am"),
-//    Meds(name: "Vitamin D", time: "8am"),
-//    Meds(name: "Vitamin C", time: "8am"),
-//    Meds(name: "Creatine", time: "5pm"),
-//    Meds(name: "Protein Suplement", time: "5pm"),
-//  ]
-    var name: String
-    var time: String
+  let name: String
+  let time: String
+
+  static let all: [Meds] =  [
+    Meds(name: "Concerta 18mg", time: "9am"),
+    Meds(name: "Vitamin D", time: "8am"),
+    Meds(name: "Vitamin C", time: "8am"),
+    Meds(name: "Creatine", time: "5pm"),
+    Meds(name: "Protein Suplement", time: "5pm"),
+  ]
 }
 
 //extension Meds: CustomStringConvertible {
