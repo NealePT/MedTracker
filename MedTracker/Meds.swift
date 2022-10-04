a5068aa303cc22597b3d32f9f76ff6acb616b5c2
Neale Taylor <nealeptaylor@gmail.com>
2022-10-04 10:59:18 -0400

    add id column to meds

--- MedTracker/Meds.swift
+++ MedTracker/Meds.swift
@@ -8,15 +8,16 @@
 import Foundation
 
 struct Meds: Codable {
-  let name: String
-  let time: String
+    let id: Int?
+    let name: String
+    let time: String
 
   static let all: [Meds] =  [
-    Meds(name: "Concerta 18mg", time: "9am"),
-    Meds(name: "Vitamin D", time: "8am"),
-    Meds(name: "Vitamin C", time: "8am"),
-    Meds(name: "Creatine", time: "5pm"),
-    Meds(name: "Protein Suplement", time: "5pm"),
+    Meds(id: 1, name: "Concerta 18mg", time: "9am"),
+    Meds(id: 2, name: "Vitamin D", time: "8am"),
+    Meds(id: 3, name: "Vitamin C", time: "8am"),
+    Meds(id: 4, name: "Creatine", time: "5pm"),
+    Meds(id: 5, name: "Protein Suplement", time: "5pm"),
   ]
 }
 
