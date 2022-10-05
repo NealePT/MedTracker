588a490ffda7e99a202393dba1becbba7c705c17
Neale Taylor <nealeptaylor@gmail.com>
2022-10-05 14:30:00 -0400

    add completed boolean to meds

--- MedTracker/Meds.swift
+++ MedTracker/Meds.swift
@@ -11,15 +11,16 @@ struct Meds: Codable {
     let id: Int?
     let name: String
     let time: String
+    let completed: Bool
 
   static let all: [Meds] =  [
-    Meds(id: 1, name: "Concerta 18mg", time: "9am"),
-    Meds(id: 2, name: "Vitamin D", time: "8am"),
-    Meds(id: 3, name: "Vitamin C", time: "8am"),
-    Meds(id: 4, name: "Vitamin B12", time: "8am"),
-    Meds(id: 5, name: "Iron Supplement", time: "8am"),
-    Meds(id: 6, name: "Creatine", time: "5pm"),
-    Meds(id: 7, name: "Protein Supplement", time: "5pm"),
+    Meds(id: 1, name: "Concerta 18mg", time: "9am", completed: false),
+    Meds(id: 2, name: "Vitamin D", time: "8am", completed: false),
+    Meds(id: 3, name: "Vitamin C", time: "8am", completed: false),
+    Meds(id: 4, name: "Vitamin B12", time: "8am", completed: false),
+    Meds(id: 5, name: "Iron Supplement", time: "8am", completed: false),
+    Meds(id: 6, name: "Creatine", time: "5pm", completed: false),
+    Meds(id: 7, name: "Protein Supplement", time: "5pm", completed: false)
   ]
 }
 
